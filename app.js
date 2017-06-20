e7042b54a77f7ebca69a3b83cb82902a09858bad
Yifeng Zhu <740116529@qq.com>
2017-06-20 10:12:43 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://yifeng:1234@ds133192.mlab.com:33192/yelpcamp_yifeng");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
